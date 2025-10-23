c98beddd2188b3c2a243661b9e37bbe87023ef4b
Enes Ago <enesago010@gmail.com>
2025-10-24 01:57:23 +0200

    beta 1.0.1

--- backend/pharmacy/admin.py
+++
@@ -1,1 +0,0 @@
-# Admin disabled - API only
--- backend/pharmacy/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import Medicine
-
-
-class MedicineSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Medicine
-        fields = ['id', 'name', 'description', 'created_at', 'updated_at']
-        read_only_fields = ['id', 'created_at', 'updated_at']
